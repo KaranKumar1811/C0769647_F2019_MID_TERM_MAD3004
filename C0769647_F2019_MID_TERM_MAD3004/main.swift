a6725a474a4a8b56f066d15c295ea4bd7c334ce5
Karan Kumar <kumarkay.ca@gmail.com>
2019-10-19 18:56:13 -0400

    Creating Object of HydroBill In Main

--- C0769647_F2019_MID_TERM_MAD3004/main.swift
+++ C0769647_F2019_MID_TERM_MAD3004/main.swift
@@ -15,5 +15,7 @@ let date_2 = formatter.date(from: "2013/04/12")?.dateFormat()
 let date_3 = formatter.date(from: "2015/02/07")?.dateFormat()
 
 var i1 = Internet(providerName: "Freedom", gbUsed: 63.2, billId: "IBill1", billDate: date_1!, billAmount: 118.0, billType: Bill.Types.Internet)
-var m1 = Mobile(manufacturerName: "Samsung", planName: "BigGig", mobileNumber: "0122344556", internetUsed: 12.4, minutesUsed: 334, billAmount: 234.5, billId: "BillM", billType:Bill.Types.Mobile, billDate: date_2!)
+var m1 = Mobile(manufacturerName: "Samsung", planName: "BigGig", mobileNumber: "0122344556", internetUsed: 12.4, minutesUsed: 334, billAmount: 234.5, billId: "MBill1", billType:Bill.Types.Mobile, billDate: date_2!)
+var h1 = Hydro(agencyName: "Toronto Power", unitsConsumed: 231.4, billAmount: 743, billId: "HBill1", billType: Bill.Types.Hydro, billDate: date_3!)
+
 
