c0c7a932926ff62c87899c9ffe490da926520131
Karan Kumar <kumarkay.ca@gmail.com>
2019-10-19 20:10:34 -0400

    Creating Objects of Insurance and customer 4

--- C0769647_F2019_MID_TERM_MAD3004/main.swift
+++ C0769647_F2019_MID_TERM_MAD3004/main.swift
@@ -14,6 +14,9 @@ let date_1 = formatter.date(from: "2012/12/02")?.dateFormat()
 let date_2 = formatter.date(from: "2013/04/12")?.dateFormat()
 let date_3 = formatter.date(from: "2015/02/07")?.dateFormat()
 
+let startDateOfInsurance = formatter.date(from: "2019/01/01")
+let endDateOfInsurance = formatter.date(from: "2019/12/30")
+let date_4 = formatter.date(from: "2018/12/30")
 var i1 = Internet(providerName: "Freedom", gbUsed: 63.2, billId: "IBill1", billDate: date_1!, billAmount: 118.0, billType: Bill.Types.Internet)
 var m1 = Mobile(manufacturerName: "Samsung", planName: "BigGig", mobileNumber: "0122344556", internetUsed: 12.4, minutesUsed: 334, billAmount: 234.5, billId: "MBill1", billType:Bill.Types.Mobile, billDate: date_2!)
 var h1 = Hydro(agencyName: "Toronto Power", unitsConsumed: 231.4, billAmount: 743, billId: "HBill1", billType: Bill.Types.Hydro, billDate: date_3!)
@@ -25,7 +28,9 @@ var c2 = Customer(customerId: "C02", firstName: "Sandeep ", lastName: "Singh", e
 
 var c3 = Customer(customerId: "C03", firstName: "Ankita ", lastName: "Jain", email: "Ankita@developer.com", billDictionary: [:])
 
+var In1 = Insurance(Type: Insurance.typeInsurance.Home, Provider: "Life Gaurd Insurance", StartDate: startDateOfInsurance!, EndDate: endDateOfInsurance!, BillId: "Insure01", BillDate: (date_4?.dateFormat())!, billAmount: 627.23, BillType: Bill.Types.Insurance)
 
+var c4 = Customer(customerId: "C33", firstName: "Ankita ", lastName: "Jain", email: "Ankita@developer.com", billDictionary: [In1.billId:In1])
 
 
 
