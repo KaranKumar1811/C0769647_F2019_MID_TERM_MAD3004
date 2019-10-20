9296bc7952ddf033e9f85794527f2b1beebef74e
Karan Kumar <kumarkay.ca@gmail.com>
2019-10-19 20:00:55 -0400

    Printing data and Setting value of Total Instalment in Insurance

--- C0769647_F2019_MID_TERM_MAD3004/Insurance.swift
+++ C0769647_F2019_MID_TERM_MAD3004/Insurance.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-class Insurance
+class Insurance:Bill
 {
     enum typeInsurance{
         case Home
@@ -19,14 +19,28 @@ class Insurance
     var startDate : Date
     var endDate : Date
     var totalInstallmentAmount :Int
+    {
+        get
+        {   let time_interval=endDate.timeIntervalSince(startDate)
+            return Int(5*time_interval)
+        }
+    }
     
     
-    init(Type: typeInsurance,Provider : String, StartDate: Date, EndDate: Date, TotalInstallment: Int) {
+    init(Type: typeInsurance,Provider : String, StartDate: Date, EndDate: Date,BillId : String, BillDate : String,billAmount: Float, BillType : Bill.Types) {
         self.typeOfInsurance=Type
         self.provider=Provider
         self.startDate=StartDate
         self.endDate=EndDate
-        self.totalInstallmentAmount=TotalInstallment
+        super.init(billId: BillId, billDate: BillDate, billType: BillType)
     }
+    
+    override func printMyData() {
+        print("******************************************************")
+        print("\nTypeOfInsurance : \(typeOfInsurance)")
+        print("Proveder: \(provider)")
+        print("Start Date : \(startDate)")
+        print("End Date : \(endDate)")
+        print("Total Installment : \(totalInstallmentAmount)\n")    }
     
 }
